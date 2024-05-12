8866b95284deec20b094facc79ac24eb9c3eed3a
Jam851 <knagrale851@gmail.com>
2024-05-12 18:16:27 +0530

    second commit

---
+++ BASICS OF JAVASCRIPT/primitiveDataTypes.js
@@ -0,0 +1,15 @@
+let name = 'Million Dollar Baby';
+let age = 20;
+let Likeness = true;
+let musicType = undefined;
+let Hate = null;
+//Different Types of Primitve Data Types
+
+console.log(name, age, Likeness, musicType, Hate);
+//Printing variables with console
+
+console.log("\nGeorgie - What is your name fooli-*whack*\nMatilda - Shut it you big oaf\n")
+//using next line in a console statement
+
+console.log(`My age is going to change from ${age} this yr to ${++age}\n`)
+//The sophisticated way of using variables in a console statement
